e0f141f340c5acdd9ce41d111be21029152aeea9
chasju <91468816+chasju@users.noreply.github.com>
2023-06-01 10:00:44 +0200

    Adds to onskeliste page

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-const burgerBtn = document.querySelector(".hamburger");
-const dropdown = document.querySelector(".dropdown");
-
-let burgerOpen = false;
-
-burgerBtn.addEventListener("click", () => {
-  if (!burgerOpen) {
-    burgerBtn.classList.add("open");
-    dropdown.style.display = "flex";
-    burgerOpen = true;
-  } else {
-    burgerBtn.classList.remove("open");
-    dropdown.style.display = "none";
-    burgerOpen = false;
-  }
-});
-
 // counter
 
 const countContainer = document.querySelector(".counter");
