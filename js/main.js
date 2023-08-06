2da979adf6497a91604b3ddd611516c65f13d8a3
chasju <91468816+chasju@users.noreply.github.com>
2023-08-06 19:04:33 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -1,36 +1,37 @@
-// counter
+// // counter
 
-const countContainer = document.querySelector(".counter");
-const weddingDate = new Date("Aug 5, 2023 13:00:00").getTime();
+// const countContainer = document.querySelector(".counter");
+// const weddingDate = new Date("Aug 5, 2023 13:00:00");
 
-let countDown = setInterval(() => {
-  const todaysDate = new Date().getTime();
-  const distance = weddingDate - todaysDate;
+// console.log(weddingDate);
+// let countDown = setInterval(() => {
+//   const todaysDate = new Date().getTime();
+//   const distance = weddingDate - todaysDate;
 
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  countContainer.innerHTML = "";
+//   countContainer.innerHTML = "";
 
-  countContainer.innerHTML = `<p class="days">${days}d</p>
-                            <p class="">${hours}t</p>
-                            <p class="mins">${minutes}m</p>
-                            <p class="secs">${seconds}s</p>`;
-}, 1000);
+//   countContainer.innerHTML = `<p class="days">${days}d</p>
+//                             <p class="">${hours}t</p>
+//                             <p class="mins">${minutes}m</p>
+//                             <p class="secs">${seconds}s</p>`;
+// }, 1000);
 
 // Reaction
 
-const reactButton = document.querySelector(".ready-container button");
-let reactP = document.querySelector(".ready-container .reaction");
+// const reactButton = document.querySelector(".ready-container button");
+// let reactP = document.querySelector(".ready-container .reaction");
 
-reactP = ["JA!"];
+// reactP = ["JA!"];
 
-reactButton.addEventListener("click", () => {
-  reactP.push("!");
-  reactButton.innerHTML = reactP.join("");
-  if (reactP.join("") === "JA!!!!!!!!!!!!!") {
-    reactP.splice(1);
-  }
-});
+// reactButton.addEventListener("click", () => {
+//   reactP.push("!");
+//   reactButton.innerHTML = reactP.join("");
+//   if (reactP.join("") === "JA!!!!!!!!!!!!!") {
+//     reactP.splice(1);
+//   }
+// });
